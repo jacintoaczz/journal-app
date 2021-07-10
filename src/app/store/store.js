9b3b6d9a10808f7bf5848cac30f7246d267e82d5
Jacinto Acosta <jacintoaczz@gmail.com>
2021-07-10 14:45:53 -0400

    Configuracion de redux-thunk - Se aplico la funcion composeWithDevtools par aplicar el middleware

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -1,4 +1,6 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import { authReducer } from "./features/auth/reducers/auth.reducer";
 
@@ -8,5 +10,5 @@ const reducers = combineReducers({
 
 export const store = createStore(
   reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools(applyMiddleware(thunk))
 );
