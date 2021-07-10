d19bfa42d592610e79653f92a7fef647ee4972a7
Jacinto Acosta <jacintoaczz@gmail.com>
2021-07-10 14:46:11 -0400

    Creacion de primera accion asincrona

--- src/app/store/features/auth/actions/auth.js
+++ src/app/store/features/auth/actions/auth.js
@@ -1,5 +1,13 @@
 import { types } from "../../../../types/types";
 
+export const loginWithEmailAndPassword = (email, password) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(login(123, "Allenz"));
+    }, 3500);
+  };
+};
+
 export const login = (uid, displayName) => {
   return {
     type: types.login,
