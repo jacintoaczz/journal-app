a6917b131079227a2a8c2e29844816de6b7a0490
Jacinto Acosta <jacintoaczz@gmail.com>
2021-07-23 14:15:40 -0400

    Feat: Implementar el UI reducer

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -3,9 +3,11 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 import { authReducer } from "./features/auth/reducers/auth.reducer";
+import { uiReducer } from "./features/ui/reducers/uiReducer";
 
 const reducers = combineReducers({
   auth: authReducer,
+  ui: uiReducer,
 });
 
 export const store = createStore(
